5ca2cb6e88796c5fb2f29f05c29e71215327ab73
Zankoku Okuno <zankoku.okuno@gmail.com>
2013-09-27 00:31:03 -0400

    better List1 definition

--- Data/List1.hs
+++ Data/List1.hs
@@ -1,17 +1,13 @@
-{-# LANGUAGE DeriveFunctor #-}
 module Data.List1 where
 
-data List1 a = Nil1 a | Cons1 a (List1 a)
-	deriving (Eq, Functor)
-
-toList1 :: [a] -> List1 a
-toList1 [] = error "Attempt to create List1 value without elements."
-toList1 [x] = Nil1 x
-toList1 (x : xs) = Cons1 x $ toList1 xs
+newtype List1 a = List1 (a, [a])
 
 fromList1 :: List1 a -> [a]
-fromList1 (Nil1 x) = [x]
-fromList1 (Cons1 x xs) = x : fromList1 xs
+fromList1 (x, [xs]) = x:xs
+
+toList1 :: [a] -> List1 a
+toList1 xs = (head xs, tail xs)
+
 
 instance Show a => Show (List1 a) where
 	show xs = "1[" ++ impl xs ++ "]"
