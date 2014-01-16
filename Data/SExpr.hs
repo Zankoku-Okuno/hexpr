431a733fb05d3bc7b13709eecf5945eb9038243b
Zankoku Okuno <zankoku.okuno@gmail.com>
2014-01-16 17:42:02 -0500

    sexpr->spine conversion

--- Data/SExpr.hs
+++ Data/SExpr.hs
@@ -2,7 +2,24 @@ module Data.SExpr (
 	  SExpr(..)
 	) where
 
+import Data.Spine
+
+
 data SExpr a = Atom a | SExpr [SExpr a]
 
 
---TODO SExpr to Spine
+class SExprToSpine a where
+	xformNull :: Spine a
+	xformNull = error "Empty sexprs are disallowed"
+	xformSingleton :: a -> Spine a
+
+	xformDeepAtom :: a -> a
+	xformDeepAtom = id
+
+sexprToSpine :: (SExprToSpine a) => SExpr a -> Spine a
+sexprToSpine (Atom x) = Leaf (xformDeepAtom x)
+sexprToSpine (SExpr []) = xformNull
+sexprToSpine (SExpr [Atom x]) = xformSingleton x
+sexprToSpine (SExpr [xs]) = sexprToSpine xs
+sexprToSpine (SExpr xs) = Branch (map sexprToSpine xs)
+
