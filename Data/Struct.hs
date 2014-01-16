bf234613d0f957e33159cd6ddf4dc1ea08f9122c
Zankoku Okuno <zankoku.okuno@gmail.com>
2014-01-16 17:50:14 -0500

    cleaned up Struct documentation

--- Data/Struct.hs
+++ Data/Struct.hs
@@ -61,6 +61,7 @@ import Data.Maybe
 import Control.Applicative
 
 
+------ Types ------
 {-| Aggregating struct data type.
     Every field of an aggregating struct must be filled.
 -}
@@ -78,18 +79,24 @@ data Struct k a = Struct [a] [(k, a)]
 -- FIXME horribly inefficient access, storage waste
 newtype Costruct k a = Costruct { unCostruct :: Struct k (Maybe a) }
 {-| Template from which a costruct may be created.
-    Generally, you would define a Costruct type, then transform the type into a
-    template for values, and use that template to create actual values.
+    Generally, you would define a @'Costruct' MyName MyType@ statically, then transform
+    this into a @'CostructTemplate' MyName@ template with 'emptySelection', and use that
+    template with 'mkSelection' to create actual @'Costruct' MyName MyValue@ values at
+    runtime.
 -}
 newtype CostructTemplate k = CostructTemplate (Struct k ())
 
+
+------ Access ------
 {-| Look up a positional field. -}
 getIx :: Struct k a -> Int -> Maybe a
 getIx struct i | hasIx struct i = Just (fieldIx struct i)
                | otherwise      = Nothing
+
 {-| Lookup a named field. -}
 getKw :: (Eq k) => Struct k a -> k -> Maybe a
 getKw (Struct _ kw) name = lookup name kw
+
 {-| Look up a positional field, but if the index goes past the positional fields,
     look into the named fields.
 -}
@@ -100,9 +107,11 @@ getEither struct i | hasEither struct i = Just (fieldEither struct i)
 {-| Check for existence of a positional field -}
 hasIx :: Struct k a -> Int -> Bool
 hasIx (Struct pos _) i = 0 <= i && i < length pos
+
 {-| Check for existence of a named field -}
 hasKw :: (Eq k) => Struct k a -> k -> Bool
 hasKw (Struct _ kw) name = name `elem` map fst kw
+
 {-| Check for existence of a positional field, but if the index goes past
     the positional fields, look into the named fields.
 -}
@@ -112,9 +121,11 @@ hasEither (Struct pos kw) i = 0 <= i && i < length pos + length kw
 {-| Look up a positional field. Fails if field does not exist. -}
 fieldIx :: Struct k a -> Int -> a
 fieldIx (Struct pos _) n = pos !! n
+
 {-| Lookup a named field. Fails if field does not exist. -}
 fieldKw :: (Eq k) => Struct k a -> k -> a
 fieldKw struct name = fromJust $ getKw struct name
+
 {-| Look up a positional field, but if the index goes past the positional fields,
     look into the named fields. Fails if field does not exist.
 -}
@@ -122,39 +133,6 @@ fieldEither :: Struct k a -> Int -> a
 fieldEither struct@(Struct pos kw) i | hasIx struct i = fieldIx struct i
                                      | otherwise      = snd $ kw !! (i - length pos)
 
-{-| Modify a positional field of a struct. No change if the field does not exist. -}
-setIx :: Struct k a -> Int -> a -> Struct k a
-setIx struct@(Struct pos kw) i v | 0 <= i && i < length pos = (Struct (replaceAt i pos v) kw)
-                                 | otherwise                = struct
-{-| Modify a named field of a struct. No change if the field does not exist. -}
-setKw :: Struct k a -> k -> a -> Struct k a
-setKw = undefined --STUB
-{-| Modify a positional field of a struct, but if the index goes past the
-    positional fields, modify in the named fields. No change if the field
-    does not exist.
--}
-setEither :: Struct k a -> Int -> a -> Struct k a
-setEither struct@(Struct pos kw) i v | 0 <= i && i < length pos   = setIx struct i v
-                                     | i - length pos < length kw = let name = fst $ kw !! (i - length pos)
-                                                                    in Struct pos (replaceAt (i - length pos) kw (name, v))
-                                     | otherwise                  = struct
-
-{-| Form a 'Struct' with all fields filled from lists of positional
-    and keyword arguments.
--}
-mkAggregate :: [a] -> [(k, a)]-> Struct k a
-mkAggregate = Struct
-
-{-| For a 'Struct' with exactly one field filled from lists of positional
-    and keyword arguments.
--}
-mkSelection :: CostructTemplate k -> Either Int k -> a -> Costruct k a
-mkSelection struct (Left i) v = Costruct $ setEither (blankTemplate struct) i (Just v)
-mkSelection struct (Right name) v = Costruct $ setKw (blankTemplate struct) name (Just v)
-
-{-| Create an 'Struct' identical to the input, but with no fields filled .-}
-emptySelection :: Struct k a -> CostructTemplate k
-emptySelection (Struct pos kw) = CostructTemplate $ Struct (map (const ()) pos) (map (\(name, v) -> (name, ())) kw)
 
 {-| Determine which field is filled in a selection struct. -}
 whichFilled :: Costruct k a -> Either Int k
@@ -182,11 +160,54 @@ getFilled (Costruct (Struct pos kw)) = case findPosition <|> findKeyword of
         [(_, v)] -> v
         _ -> Nothing
 
+
+------ Modification ------
+{-| Modify a positional field of a struct. No change if the field does not exist. -}
+setIx :: Struct k a -> Int -> a -> Struct k a
+setIx struct@(Struct pos kw) i v | 0 <= i && i < length pos = (Struct (replaceAt i pos v) kw)
+                                 | otherwise                = struct
+
+{-| Modify a named field of a struct. No change if the field does not exist. -}
+setKw :: Struct k a -> k -> a -> Struct k a
+setKw = undefined --STUB
+
+{-| Modify a positional field of a struct, but if the index goes past the
+    positional fields, modify in the named fields. No change if the field
+    does not exist.
+-}
+setEither :: Struct k a -> Int -> a -> Struct k a
+setEither struct@(Struct pos kw) i v | 0 <= i && i < length pos   = setIx struct i v
+                                     | i - length pos < length kw = let name = fst $ kw !! (i - length pos)
+                                                                    in Struct pos (replaceAt (i - length pos) kw (name, v))
+                                     | otherwise                  = struct
+
 {-| Update the unique filled slot of a selection struct to the passed value. -}
 setFilled :: Costruct k a -> a -> Costruct k a
 setFilled struct v = Costruct $ case whichFilled struct of
     Left i -> setIx (unCostruct struct) i (Just v)
     Right name -> setKw (unCostruct struct) name (Just v)
+
+
+------ Creation ------
+{-| Form a 'Struct' with all fields filled from lists of positional
+    and keyword arguments.
+
+    Provided for symmetry with 'mkSelection'.
+-}
+mkAggregate :: [a] -> [(k, a)]-> Struct k a
+mkAggregate = Struct
+
+{-| For a 'Struct' with exactly one field filled from lists of positional
+    and keyword arguments.
+-}
+mkSelection :: CostructTemplate k -> Either Int k -> a -> Costruct k a
+mkSelection struct (Left i) v = Costruct $ setEither (blankTemplate struct) i (Just v)
+mkSelection struct (Right name) v = Costruct $ setKw (blankTemplate struct) name (Just v)
+
+{-| Create a 'CostructTemplate' with structure identical to the input. -}
+emptySelection :: Struct k a -> CostructTemplate k
+emptySelection (Struct pos kw) = CostructTemplate $ Struct (map (const ()) pos) (map (\(name, v) -> (name, ())) kw)
+
 
 
 
